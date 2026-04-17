496256d455e977cde23f49af156190e2d44ada8f
JiyanneKateTuazon <jktuazon1009@gmail.com>
2026-04-17 14:48:55 -0400

    Add project files.

---
+++ StringExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace StringExtensionsLib
+{
+    public static class StringExtensions
+    {
+        public static bool StartsWithUpperCase(this string text)
+        {
+            if (string.IsNullOrEmpty(text))
+                return false;
+
+            return char.IsUpper(text[0]);
+        }
+    }
+}
